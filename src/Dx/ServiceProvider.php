9eb72d0fca2b00e2a906ccdb83aef5130e9e7c7e
dennesabing <dennes.b.abing@gmail.com>
2015-05-19 09:29:09 +0800

    -- Init

---
+++ src/Dx/ServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * DxLaravel Package
+ *
+ * @link http://laravel.dennesabing.com
+ * @author Dennes B Abing <dennes.b.abing@gmail.com>
+ * @license proprietary
+ * @copyright Copyright (c) 2015 ClaremontDesign/MadLabs-Dx
+ * @version 0.0.0.1
+ * @since 2015-05-18 04:30 PM
+ * @project dxlaravel
+ * @package dxlaravel
+ */
+
+namespace Dx;
+
+use Illuminate\Support\ServiceProvider as Sp;
+
+class ServiceProvider extends Sp
+{
+
+	/**
+	 * Bootstrap the Dx.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+	}
+
+	/**
+	 * Register the Dx services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
---
+++ tests/Dx/ServiceProviderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * DxLaravel Package
+ *
+ * @link http://laravel.dennesabing.com
+ * @author Dennes B Abing <dennes.b.abing@gmail.com>
+ * @license proprietary
+ * @copyright Copyright (c) 2015 ClaremontDesign/MadLabs-Dx
+ * @version 0.0.0.1
+ * @since 2015-05-18 04:30 PM
+ * @project dxlaravel
+ * @package dxlaravel
+ */
+
+namespace Dx\Tests;
+
+class ServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+
+}
